f076621a296e9585d1cd78586cf3b62d406ff44b
Maikon <euu@example.com>
2024-04-07 14:10:09 -0300

    Services created

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ const clientesRouter = require('./routes/clientes');
 
 var app = express();
 
+const dotenv = require('dotenv');
+
+dotenv.config();
+console.log(process.env.MYSQL_DB);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
