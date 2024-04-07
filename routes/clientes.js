121d0205afaabc0e18853a72ed57c36171e2d751
Maikon <euu@example.com>
2024-04-07 13:57:35 -0300

    Created routes

---
+++ routes/clientes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+/* GET clientes*/
+router.get('/', function (req, res, next) {
+    res.send('GET clientes');
+});
+/* POST clientes*/
+router.post('/', function (req, res, next) {
+    res.send('POST clientes');
+});
+/* PUT clientes*/
+router.put('/', function (req, res, next) {
+    res.send('PUT clientes');
+});
+/* DELETE clientes*/
+router.delete('/', function (req, res, next) {
+    res.send('DELETE clientes');
+});
+module.exports = router;
